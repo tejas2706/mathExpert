040c3262329405533c3236520ed35945e1824765
tejaslad <tejaslad2706@gmail.com>
2021-08-13 18:40:21 +0530

    changed standard selection in content

--- src/Pages/Content/classesAndExams.js
+++ src/Pages/Content/classesAndExams.js
@@ -1,24 +1,24 @@
 const classToExamIdMapping = [
-  {
-    id: '1',
-    class: '1',
-    examIds: ['NTSE_1', 'IMO_1', 'JMO_1'],
-  },
-  {
-    id: '2',
-    class: '2',
-    examIds: ['NTSE_2', 'IMO_2', 'JMO_2'],
-  },
-  {
-    id: '3',
-    class: '3',
-    examIds: ['NTSE_3', 'IMO_3', 'JMO_3'],
-  },
-  {
-    id: '4',
-    class: '4',
-    examIds: ['NTSE_4', 'IMO_4', 'JMO_4'],
-  },
+  // {
+  //   id: '1',
+  //   class: '1',
+  //   examIds: ['NTSE_1', 'IMO_1', 'JMO_1'],
+  // },
+  // {
+  //   id: '2',
+  //   class: '2',
+  //   examIds: ['NTSE_2', 'IMO_2', 'JMO_2'],
+  // },
+  // {
+  //   id: '3',
+  //   class: '3',
+  //   examIds: ['NTSE_3', 'IMO_3', 'JMO_3'],
+  // },
+  // {
+  //   id: '4',
+  //   class: '4',
+  //   examIds: ['NTSE_4', 'IMO_4', 'JMO_4'],
+  // },
   {
     id: '5',
     class: '5',
